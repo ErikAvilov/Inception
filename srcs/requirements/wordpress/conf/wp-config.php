b722aa143a45ef0ac929af85fae0c81d014171d7
ea <ea@debian>
2023-04-04 17:22:17 +0200

    test

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,40 @@
+<?php
+
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'Database' );
+
+/** MySQL database username */
+define( 'DB_USER', 'eavilov' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', '1234' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'host' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/** Authentication Unique Keys and Salts. */
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', __DIR__ . '/' );
+}
+
+require_once ABSPATH . 'wp-settings.php';
+
